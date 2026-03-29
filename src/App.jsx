353ade3289036591e694894357d9ddf23ffde811
KandimallaVarun <varunkandimalla333@gmail.com>
2026-03-29 15:27:18 +0530

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Auth from './pages/Auth'
 import Checkout from './pages/Checkout'
 
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
       <div className="App">
+        <Navbar />
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/auth" element={<Auth />} />
